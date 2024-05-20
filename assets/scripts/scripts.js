8aaae335b63c3a70a8b9c009fa4043975f9acb0a
E Hammond <ebaal8226@gmail.com>
2024-05-20 09:08:53 +0000

    add comments to function templates

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,31 +1,32 @@
+//-------function to print the first letter of word as capital to the DOM--------
 function capitalise(){
 
 };
-
+//--------changes game type to ask which country is this capital from-------
 function changeGame(){
 
 };
-
+//--------- game runnig function will run when page id loaded and when "next question" is clicked
 function playGame(){
 
 };
-
-function lifeRemoval(){
-
-};
-
+//------ checks value of answer if correct wrong or invalid --------
 function answerCheck(){
 
 };
+//---------removes one life for wrong answer ---------------------
+function lifeRemoval(){
 
+};
+// -------- creates message to display to user if answer correct or not--------
 function message(){
 
 };
-
+//-------- adds a point for correct answer subbtracts a point for incorrect answer
 function incrementScore(){
 
 };
-
+// ------displays game over message when no more questions or no remaining lives -----
 function gameOver(){
 
 };
