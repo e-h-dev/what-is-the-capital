6f2b0a55a24f1d87ff41835cd584303684eb71c5
E Hammond <ebaal8226@gmail.com>
2024-05-21 11:57:46 +0000

    add code to Jquery

--- assets/scripts/level-1.js
+++ assets/scripts/level-1.js
@@ -187,13 +187,12 @@ $(document).ready(function(){
       $("ul").css("background-color", "rgba(255, 170, 51, 0.5)");
       $(".game-area-background").css("background-color", "rgba(255, 170, 51, 0.5)");
       //$("div").css("color", "red");
-      $("button").css("background-color", "rgb(255, 170, 52)");
-      $("button").css("color", "red");
-      $("input").css("background-color", "rgb(255, 170, 52)");
-      $("input").css("color", "red");
+      $("button").css({"background-color": "rgb(255, 170, 52)", "color": "red"});
+      $("input").css({"background-color": "rgb(255, 170, 52)", "color": "red"});
       $("h1").css("color", "red");
       $("h3").css("background-color", "rgba(255, 170, 52, 0.7)");
       $(".lives-section-level-2").css("background-color", "rgba(255, 170, 52, 0.7)");
+      $("#theme").html("Theme 1");
     });
   });
 
