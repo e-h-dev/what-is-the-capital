cad1b4e7ec6098d63f1d8831f95adc50b8424593
E Hammond <ebaal8226@gmail.com>
2024-05-23 01:19:35 +0000

    add comments to level-1.js

--- assets/scripts/level-1.js
+++ assets/scripts/level-1.js
@@ -1,20 +1,22 @@
+/*this section at the head of the script is for 
+creating the neccaserry variables of arrays and
+DOM elements*/ 
+
 //------main game lists
 let countries = ["afghanistan","albania","algeria","andorra","angola","antigua and barbuda","argentina","armenia","australia","austria","azerbaijan","bahamas","bahrain","bangladesh","barbados","belarus","belgium","belize","benin","bhutan","bolivia","bosnia and herzegovina","botswana","brazil","brunei","bulgaria","burkina faso","burundi","cambodia","cameroon","canada","cape verde","central african republic","chad","chile","china","colombia","comoros","congo","costa rica","ivory coast","croatia","cuba","cyprus","czech republic","denmark","djibouti","dominica","dominican republic","east timor","ecuador","egypt","el salvador","england","equatorial guinea","eritrea","estonia","eswatini","ethiopia","federated states of micronesia","fiji","finland","france","gabon","gambia","georgia","germany","ghana","greece","grenada","guatemala","guinea","guinea bissau","guyana","haiti","honduras","hungary","iceland","india","indonesia","iran","iraq","ireland","israel","italy","jamaica","japan","jordan","kazakhstan","kenya","kiribati","kosovo","kuwait","kyrgyzstan","laos","latvia","lebanon","lesotho","liberia","libya","liechtenstein","lithuania","luxembourg","madagascar","malawi","malaysia","maldives","mali","malta","marshall islands","mauritania","mauritius","mexico","moldova","monaco","mongolia","montenegro","morocco","mozambique","myanmar (burma)","namibia","nauru","nepal","netherlands","new zealand","nicaragua","niger","nigeria","north korea","macedonia","northern ireland","norway","oman","pakistan","palau","palestine","panama","papua new guinea","paraguay","peru","philippines","poland","portugal","qatar","romania","russia","rwanda","saint kitts and nevis","saint lucia","saint vincent and the grenadines","samoa","san marino","sao tome and principe","saudi arabia","scotland","senegal","serbia","seychelles","sierra leone","singapore","slovakia","slovenia","solomon islands","somalia","south africa","south korea","south sudan","spain","sri lanka","sudan","suriname","sweden","switzerland","syria","taiwan","tajikistan","tanzania","thailand","togo","tonga","trinidad and tobago","tunisia","türkiye (turkey)","turkmenistan","tuvalu","uganda","ukraine","united arab emirates","united kingdom","united states","uruguay","uzbekistan","vanuatu","vatican city","venezuela","vietnam","wales","yemen","zambia","zimbabwe"]
 let capitals = ["kabul","tirana","algiers","andorra la vella","luanda","saint john's","buenos aires","yerevan","canberra","vienna","baku","nassau","manama","dhaka","bridgetown","minsk","brussels","belmopan","porto novo","thimphu","sucre","sarajevo","gaborone","brasilia","bandar seri begawan","sofia","ouagadougou","gitega","phnom penh","yaounde","ottawa","praia","bangui","n'djamena","santiago","beijing","bogota","moroni","brazzaville","san jose","yamoussoukro","zagreb","havana","nicosia","prague","copenhagen","djibouti","roseau","santo domingo","dili","quito","cairo","san salvador","london","malabo","asmara","tallinn","mbabane","addis ababa","palikir","suva","helsinki","paris","libreville","banjul","tbilisi","berlin","accra","athens","saint george's","guatemala city","conakry","bissau","georgetown","port au prince","tegucigalpa","budapest","reykjavik","new delhi","jakarta","tehran","baghdad","dublin","jerusalem","rome","kingston","tokyo","amman","astana","nairobi","tarawa atoll","pristina","kuwait city","bishkek","vientiane","riga","beirut","maseru","monrovia","tripoli","vaduz","vilnius","luxembourg","antananarivo","lilongwe","kuala lumpur","male","bamako","valletta","majuro","nouakchott","port louis","mexico city","chisinau","monaco","ulaanbaatar","podgorica","rabat","maputo","nay pyi taw","windhoek","yaren","kathmandu","amsterdam","wellington","managua","niamey","abuja","pyongyang","skopje","belfast","oslo","muscat","islamabad","melekeok","jerusalem","panama city","port moresby","asuncion","lima","manila","warsaw","lisbon","doha","bucharest","moscow","kigali","basseterre","castries","kingstown","apia","san marino","sao tome","riyadh","edinburgh","dakar","belgrade","victoria","freetown","singapore","bratislava","ljubljana","honiara","mogadishu","cape town","seoul","juba","madrid","sri jayawardenapura kotte","khartoum","paramaribo","stockholm","bern","damascus","taipei","dushanbe","dodoma","bangkok","lome","nuku'alofa","port of spain","tunis","ankara","ashgabat","funafuti","kampala","kiev","abu dhabi","london","washington d.c.","montevideo","tashkent","port vila","vatican city","caracas","hanoi","cardiff","sana'a","lusaka","harare"]
 
+// ------list of random cities to fill short lists as they empty
 let spareList = ["melbourne", "lakewood", "bangor", "dublin", "york", "newcastle", "windermere", "dresden", "Manchester", "Tel Aviv", "Boston", "Cairo"]
-console.log(spareList);
 
+//------- Dom element variables
 let next = document.getElementById("next");
-  //-------creating relavant number  
-let numlen = countries.length;
-
-//----------initial question
 let question = document.getElementById("question");
-
 let divSpace = document.getElementById("incorrect-choice");
+let message = document.getElementById("multiple-choice");
 
-message = document.getElementById("multiple-choice");
+//-------number of countries variable
+let numlen = countries.length;
 
 // ---------variables for the lives section ----------------------
 let life1 = document.getElementById("life1");
@@ -25,8 +27,7 @@ let life4 = document.getElementById("life4");
 let listOfLives = [life1, life2, life3, life4];
 let lifeNumber = 4;
 
-//fisher and yates shuffle (stack overflow) https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-
+//----------fisher and yates shuffle logic
 function randomise(myList){
     let size = myList.length;
     while(size != 0){
@@ -74,6 +75,7 @@ function lifeRemoval(){
     };
 };
 
+// ---------game over function for end of lives and end of question list
 function gameOver(){
     let oldScore = parseInt(document.getElementById('score').innerText);
     question.innerHTML = "Game over!";
@@ -81,6 +83,7 @@ function gameOver(){
     next.remove();
 };
 
+// --------shows incorrect message 1 second after user answered
 function show(myClick){
     document.getElementById(myClick).style.backgroundColor = "red";
     setTimeout(delayMessage, 1000);
@@ -91,6 +94,7 @@ function show(myClick){
         subtractScore();
     };
 
+    // --------shows correct message 1 second after user answered
 function answer(myClick){
     document.getElementById(myClick).style.backgroundColor = "green";
     setTimeout(delayMessage, 1000);
@@ -100,6 +104,7 @@ function answer(myClick){
          incrementScore();
     };
 
+// -------------main function which runs the game
 function runGame(){
     
     let arrayPosition = Math.floor(Math.random() *countries.length);
@@ -113,12 +118,14 @@ function runGame(){
     let chosenCountry = countries[arrayPosition];
     let chosenCapital = capitals[arrayPosition];
     
+    // ---------innerHTML of messages to be displayed
     wrongMessage = `<h2 id="incorrect-message-box">Sorry that is incorrect, the capital of ${chosenCountry} is <mark>${chosenCapital}!</mark> better luck next time</h2>`
     correctMessage = `<h2 id="correct-message-box">Well Done! the Capital of ${chosenCountry} is <mark>${chosenCapital}!</mark></h2>`
+    
+    //--------------chooses the question------
     question.innerHTML =  `What is the capital of ${chosenCountry}? `;
 
     //--------------------- remove chosenCapital --------------------
-    
     let remove = arrayPosition;
     console.log(remove);
    
@@ -128,51 +135,46 @@ function runGame(){
     console.log(usedQuestion);
     console.log(countries);
     console.log(capitals);
+
     //------------- create shorter lists for the wrong answer options(will not have double of the same option)
-   
     let shortList1 = capitals.slice(0,4);
     let shortList2 = capitals.slice(4,8);
     let shortList3 = capitals.slice(8,12);
-    //-------------------create random number for short list item
 
+    //-------------------create random number for short list item
     let wrongCap1 = shortList1[Math.floor(Math.random() *shortList1.length)];
     let wrongCap2 = shortList2[Math.floor(Math.random() *shortList2.length)];
     let wrongCap3 = shortList3[Math.floor(Math.random() *shortList3.length)];
 
     let list = [wrongCap1, wrongCap2, wrongCap3];
 
-    console.log(list);
-
+    // -------------length of possible answers list (three for the three incorrect answers)
     let listlen = list.length;
 
+    // ------------chooses random Capital to br displayed in the multiple chice section
     let newArrayPosition = Math.floor(Math.random() *spareList.length)
-
     capitals.push(spareList[newArrayPosition]);
-    
     spareList.splice(newArrayPosition, 1);
 
-    console.log(spareList);
-
-    console.log(shortList1);
-    console.log(shortList2);
-    console.log(shortList3);
-//--------map element to loop through each list and recreate the inner html
+    //--------map element to loop through each list and recreate the inner html
     const result2 = shortList1.map(function(cap){return `<div class="incorrect" id="result2" onclick="show('result2')">${cap}</div>`});
     const result3 = shortList2.map(function(cap){return `<div class="incorrect" id="result3" onclick="show('result3')">${cap}</div>`});
     const result4 = shortList3.map(function(cap){return `<div class="incorrect" id="result4" onclick="show('result4')">${cap}</div>`});
 
-    //---------- creates the answer box for correct answer
+    //---------- creates the multiple choice box for correct answer
     let correctResult = `<div class="incorrect" id="result1" onclick="answer('result1')">${chosenCapital}</div>`
+    
+    // --------- chooses position of each possible answer
     let result1_1 = correctResult;
     let result2_2 = result2[Math.floor(Math.random() *listlen)];
     let result3_3 = result3[Math.floor(Math.random() *listlen)];
     let result4_4 = result4[Math.floor(Math.random() *listlen)];
-
     let listx = [result1_1, result2_2, result3_3, result4_4];
 
+    //---------function to place each multiple choice option in a random position
     randomise(listx);
 
-    console.log(listx.join(""));
+    //-----joins all the multiple choice options in to DOM element for display
     divSpace.innerHTML = listx.join("");
 };
 document.addEventListener("DOMContentLoaded", runGame);
